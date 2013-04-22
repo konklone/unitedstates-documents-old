b48f48732aed029ec38b5e2fd659f0a795d5cd23
Eric Mill <konklone@gmail.com>
2013-04-22 17:51:40 -0400

    resolutions are different

--- bills/bills.rb
+++ bills/bills.rb
@@ -5,67 +5,71 @@ require 'bundler/setup'
 
 require 'nokogiri'
 
-class Bills
+module UnitedStates
+  module Documents
+    class Bills
 
-  # elements to be turned into divs (must be listed explicitly)
-  BLOCKS = %w{
-    legis-body title
-    section subsection paragraph subparagraph subchapter 
-    quoted-block
-    toc toc-entry
-  }
+      # elements to be turned into divs (must be listed explicitly)
+      BLOCKS = %w{
+        legis-body resolution-body title
+        section subsection paragraph subparagraph subchapter 
+        quoted-block
+        toc toc-entry
+      }
 
-  # elements to be turned into spans (unlisted elements default to inline)
-  INLINES = %w{
-    after-quoted-block quote
-    internal-xref external-xref
-    text header enum
-    short-title official-title
-  }
+      # elements to be turned into spans (unlisted elements default to inline)
+      INLINES = %w{
+        after-quoted-block quote
+        internal-xref external-xref
+        text header enum
+        short-title official-title
+      }
 
-  # Given a path to an XML file published by the House or Senate,
-  # produce an HTML version of the document at the given output.
-  def self.process(text, options = {})
-    doc = Nokogiri::XML text
+      # Given a path to an XML file published by the House or Senate,
+      # produce an HTML version of the document at the given output.
+      def self.process(text, options = {})
+        doc = Nokogiri::XML text
 
-    # let's start by just caring about the body of the bill - the legis-body
-    body = doc.at "legis-body"
-    body.traverse do |node|
+        # let's start by just caring about the body of the bill - the legis-body
+        body = doc.at("legis-body") || doc.at("resolution-body")
+        body.traverse do |node|
 
-      # for some nodes, we'll preserve some attributes
-      preserved = {}
+          # for some nodes, we'll preserve some attributes
+          preserved = {}
 
-      # <external-xref legal-doc="usc" parsable-cite="usc/12/5301"
-      # cite check
-      if (node.name == "external-xref") and (node.attributes["legal-doc"].value == "usc")
-        preserved["data-citation-type"] = "usc"
-        preserved["data-citation-id"] = node.attributes["parsable-cite"].value
+          # <external-xref legal-doc="usc" parsable-cite="usc/12/5301"
+          # cite check
+          if (node.name == "external-xref") and (node.attributes["legal-doc"].value == "usc")
+            preserved["data-citation-type"] = "usc"
+            preserved["data-citation-id"] = node.attributes["parsable-cite"].value
+          end
+
+          # turn into a div or span with a class of its old name
+          name = node.name
+          if BLOCKS.include?(name)
+            node.name = "div"
+          else # inline
+            node.name = "span"
+          end
+          preserved["class"] = name
+
+
+          # strip out all attributes
+          node.attributes.each do |key, value|
+            node.attributes[key].remove
+          end
+
+          # restore just the ones we were going to preserve
+          preserved.each do |key, value|
+            node.set_attribute key, value
+          end
+        end
+
+        body.to_html
       end
 
-      # turn into a div or span with a class of its old name
-      name = node.name
-      if BLOCKS.include?(name)
-        node.name = "div"
-      else # inline
-        node.name = "span"
-      end
-      preserved["class"] = name
-
-
-      # strip out all attributes
-      node.attributes.each do |key, value|
-        node.attributes[key].remove
-      end
-
-      # restore just the ones we were going to preserve
-      preserved.each do |key, value|
-        node.set_attribute key, value
-      end
     end
-
-    body.to_html
   end
-
 end
 
 if $0 == __FILE__
@@ -94,5 +98,5 @@ if $0 == __FILE__
   outfile = options.delete :out
   text = File.read infile
 
-  puts Bills.process text, options
+  puts UnitedStates::Documents::Bills.process text, options
 end
