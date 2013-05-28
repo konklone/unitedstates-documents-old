46aa5c4d78dac284e22ac221da319d715453bc44
Parker Moore <parkrmoore@gmail.com>
2013-05-28 22:26:34 +0200

    Refactored UnitedStates::Documents::Bills into more manageable bits

--- bills/bills.rb
+++ bills/bills.rb
@@ -37,37 +37,97 @@ module UnitedStates
           end
 
           # for some nodes, we'll preserve some attributes
-          preserved = {}
-
-          # <external-xref legal-doc="usc" parsable-cite="usc/12/5301"
-          # cite check
-          if (node.name == "external-xref") and (node.attributes["legal-doc"] and (node.attributes["legal-doc"].value == "usc"))
-            preserved["data-citation-type"] = "usc"
-            preserved["data-citation-id"] = node.attributes["parsable-cite"].value
-          end
-
           # turn into a div or span with a class of its old name
-          name = node.name
-          if BLOCKS.include?(name)
-            node.name = "div"
-          else # inline
-            node.name = "span"
-          end
-          preserved["class"] = name
-
-
-          # strip out all attributes
-          node.attributes.each do |key, value|
-            node.attributes[key].remove
-          end
-
-          # restore just the ones we were going to preserve
-          preserved.each do |key, value|
-            node.set_attribute key, value
-          end
+          node = xml_to_html(node)
         end
 
         body.to_html
+      end
+
+      # Static: Preserve citation for usc legal docs
+      #
+      # node      - the Nokogiri::XML node
+      # preserved - the Hash of preserved values
+      #
+      # Examples:
+      #   node: <external-xref legal-doc="usc" parsable-cite="usc/12/5301" ... />
+      #   cite_check(node)
+      #   # => {
+      #   #  "data-citation-type" => "usc",
+      #   #  "data-citation-id"   => "usc/12/5301"
+      #   # }
+      #
+      # Returns a new hash with data-citation-type
+      #   and data-citation-id set to the given values in the external-xref
+      #   if it is a USC legal doc and this node is an external-xref one
+      def self.citations(node)
+        preserved = {}
+        if external_xref?(node) and usc_legal_doc?(node)
+          preserved["data-citation-type"] = "usc"
+          preserved["data-citation-id"]   = node.attributes["parsable-cite"].value
+        end
+        preserved
+      end
+
+      # Static: Determined if a node is an external-xref node
+      #
+      # node - the Nokogiri::XML node
+      #
+      # Returns true if the node is an external-xref node, false otherwise
+      def self.external_xref?(node)
+        node.name == "external-xref"
+      end
+
+      # Static: Determined if a node is a node representing the ref to a USC legal doc
+      #
+      # node - the Nokogiri::XML node
+      #
+      # Returns true if the node is a node representing a USC legal doc, false otherwise
+      def self.usc_legal_doc?(node)
+        node.attributes["legal-doc"] &&
+          node.attributes["legal-doc"].value == "usc"
+      end
+
+      # Static: Transform the XML node to an HTML one
+      #
+      # node - the Nokogiri::XML node from the XML doc
+      #
+      # Returns the HTML node with the proper attributes
+      def self.xml_to_html(node)
+        preserved = Hash.new
+          .merge(citations(node))
+          .merge({"class" => node.name})
+        node.name = html_node_name(node)
+        replace_attributes(node, preserved)
+      end
+
+      # Static: Fetch the corresponding HTML tag name for the given node
+      #
+      # node - the Nokogiri::XML node
+      #
+      # Returns the proper HTML name for the given node 
+      def self.html_node_name(node)
+        BLOCKS.include?(node.name) ? "div" : "span"
+      end
+
+      # Static: Strip out all the node's attributes and set those which were preserved
+      #
+      # node      - the Nokogiri::XML node
+      # preserved - the hash of preserved node attributes
+      # 
+      # Returns the node with just the attributes which were in the preserved Hash
+      def self.replace_attributes(node, preserved)
+        # strip out all attributes
+        node.attributes.each do |key, value|
+          node.attributes[key].remove
+        end
+
+        # restore just the ones we were going to preserve
+        preserved.each do |key, value|
+          node.set_attribute key, value
+        end
+
+        node
       end
 
     end
