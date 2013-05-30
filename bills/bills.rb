c7a32b4cbdf82c82b035c076b203a1dc9b628737
Eric Mill <konklone@gmail.com>
2013-05-30 15:05:28 -0400

    Tone down some of the comments, merge a little code back inline, but preserve the broken-out-ness where it makes sense

--- bills/bills.rb
+++ bills/bills.rb
@@ -7,7 +7,7 @@ module UnitedStates
       # elements to be turned into divs (must be listed explicitly)
       BLOCKS = %w{
         bill form amendment-form engrossed-amendment-form resolution-form
-        legis-body resolution-body engrossed-amendment-body amendment-body 
+        legis-body resolution-body engrossed-amendment-body amendment-body
         title
         amendment amendment-block amendment-instruction
         section subsection paragraph subparagraph subchapter clause
@@ -36,86 +36,46 @@ module UnitedStates
             next
           end
 
-          # for some nodes, we'll preserve some attributes
-          # turn into a div or span with a class of its old name
-          node = xml_to_html(node)
+          # preserve the node's old name as its class
+          preserved = {"class" => node.name}
+
+          # break out any detected cites
+          preserved.merge! citations(node)
+
+          # switch the name to a div or span
+          node.name = html_node_name node
+
+          # strip out extraneous attributes
+          replace_attributes node, preserved
         end
 
         body.to_html
       end
 
-      # Static: Preserve citation for usc legal docs
-      #
-      # node      - the Nokogiri::XML node
-      # preserved - the Hash of preserved values
-      #
-      # Examples:
-      #   node: <external-xref legal-doc="usc" parsable-cite="usc/12/5301" ... />
-      #   cite_check(node)
-      #   # => {
-      #   #  "data-citation-type" => "usc",
-      #   #  "data-citation-id"   => "usc/12/5301"
-      #   # }
-      #
-      # Returns a new hash with data-citation-type
-      #   and data-citation-id set to the given values in the external-xref
-      #   if it is a USC legal doc and this node is an external-xref one
+
+      # Break out and preserve detected citation details
+      # e.g. a parsable-cite of usc/12/5301 yields:
+      #   {
+      #    "data-citation-type" => "usc",
+      #    "data-citation-id"   => "usc/12/5301"
+      #   }
       def self.citations(node)
-        preserved = {}
-        if external_xref?(node) and usc_legal_doc?(node)
-          preserved["data-citation-type"] = "usc"
-          preserved["data-citation-id"]   = node.attributes["parsable-cite"].value
+        citations = {}
+        if (node.name == "external-xref") and
+          (node.attributes["legal-doc"] && node.attributes["legal-doc"].value == "usc")
+          citations["data-citation-type"] = "usc"
+          citations["data-citation-id"]   = node.attributes["parsable-cite"].value
         end
-        preserved
+        citations
       end
 
-      # Static: Determined if a node is an external-xref node
-      #
-      # node - the Nokogiri::XML node
-      #
-      # Returns true if the node is an external-xref node, false otherwise
-      def self.external_xref?(node)
-        node.name == "external-xref"
-      end
 
-      # Static: Determined if a node is a node representing the ref to a USC legal doc
-      #
-      # node - the Nokogiri::XML node
-      #
-      # Returns true if the node is a node representing a USC legal doc, false otherwise
-      def self.usc_legal_doc?(node)
-        node.attributes["legal-doc"] &&
-          node.attributes["legal-doc"].value == "usc"
-      end
-
-      # Static: Transform the XML node to an HTML one
-      #
-      # node - the Nokogiri::XML node from the XML doc
-      #
-      # Returns the HTML node with the proper attributes
-      def self.xml_to_html(node)
-        preserved = Hash.new
-          .merge(citations(node))
-          .merge({"class" => node.name})
-        node.name = html_node_name(node)
-        replace_attributes(node, preserved)
-      end
-
-      # Static: Fetch the corresponding HTML tag name for the given node
-      #
-      # node - the Nokogiri::XML node
-      #
-      # Returns the proper HTML name for the given node 
+      # Fetch the corresponding HTML tag name for the given node
       def self.html_node_name(node)
         BLOCKS.include?(node.name) ? "div" : "span"
       end
 
-      # Static: Strip out all the node's attributes and set those which were preserved
-      #
-      # node      - the Nokogiri::XML node
-      # preserved - the hash of preserved node attributes
-      # 
-      # Returns the node with just the attributes which were in the preserved Hash
+      # Strip out all the node's attributes and set those which were preserved
       def self.replace_attributes(node, preserved)
         # strip out all attributes
         node.attributes.each do |key, value|
