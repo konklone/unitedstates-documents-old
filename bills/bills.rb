15c069fc7567fc412a4d9888598e0dd007f0808c
Eric Mill <konklone@gmail.com>
2013-05-30 17:58:38 -0400

    Made the 'add blocks' functionality optional, and refactored to put it as a first step before the main processor. Also stopped using #traverse, and started iterating through all nodes.

--- bills/bills.rb
+++ bills/bills.rb
@@ -5,8 +5,6 @@ module UnitedStates
     class Bills
 
       # elements to be turned into divs (must be listed explicitly).
-      # these will also be used to advance a document counter that numbers
-      # each block, to give identifiers per-displayable unit.
       BLOCKS = %w{
         bill form amendment-form engrossed-amendment-form resolution-form
         legis-body resolution-body engrossed-amendment-body amendment-body
@@ -15,18 +13,31 @@ module UnitedStates
         section subsection paragraph subparagraph subchapter clause
         quoted-block
         toc toc-entry
+        p
       }
+
+      # to always be surrounded with a p tag when blocks mode is on
+      DISPLAY_BLOCKS = %w{ continuation-text official-title }
 
       # Given a path to an XML file published by the House or Senate,
       # produce an HTML version of the document at the given output.
       def self.process(text, options = {})
+
         doc = Nokogiri::XML text
 
-        # document counter - number units as the document proceeds
+
+        # first (optional) pass: structural alteration
+        if options[:blocks]
+          add_blocks doc
+        end
+
+
+        # main pass: process all nodes
+
+        # document counter - number any existing block units from previous pass
         counter = 0
 
-        body = doc.root
-        body.traverse do |node|
+        doc.search("*").each do |node|
 
           if node.name == "metadata"
             node.remove
@@ -36,20 +47,22 @@ module UnitedStates
           # preserve the node's old name as its class
           preserved = {"class" => node.name}
 
-          # detect this node's place in the hierarchy
-          if block?(node)
-            counter += 1
-            data = {"data-block" => counter}
-
-            if options[:hierarchy].is_a?(Proc)
-              data["data-block"] = options[:hierarchy].call counter
-            end
-
-            preserved.merge! data
-          end
-
           # break out any detected cites
           preserved.merge! citations(node)
+
+
+          if node.name == "p"
+            counter += 1
+
+            if options[:block_id].is_a?(Proc)
+              id = options[:block_id].call counter
+            else
+              id = counter
+            end
+
+            preserved.merge! "data-block-id" => id
+          end
+
 
           # switch the name to a div or span
           node.name = html_node_name node
@@ -70,7 +83,7 @@ module UnitedStates
           end
         end
 
-        body.to_html
+        doc.root.to_html
       end
 
 
@@ -92,11 +105,49 @@ module UnitedStates
 
       # Fetch the corresponding HTML tag name for the given node
       def self.html_node_name(node)
-        block?(node) ? "div" : "span"
+        BLOCKS.include?(node.name) ? "div" : "span"
       end
 
-      def self.block?(node)
-        BLOCKS.include? node.name
+      def self.containerify(doc, nodes)
+        nodes = [nodes] unless nodes.is_a?(Array)
+        p = Nokogiri::XML::Node.new "p", doc
+        nodes.first.add_previous_sibling p
+
+        nodes.each {|node| node.parent = p}
+      end
+
+      def self.add_blocks(doc)
+        # put some things in containers that need to be for meaningful display.
+        doc.search("*").each do |node|
+          # if the node's an <enum>, and it's not in a title or section -
+          # scoop up it and any immediate sibling <header> and <text> elements,
+          # and put them inside a container of <div class="p">.
+          if node.name == "enum"
+            unless ["section", "title"].include?(node.parent.name)
+              brother_header, brother_text = [nil, nil]
+
+              if node.next_sibling.name == "header"
+                brother_header = node.next_sibling
+              end
+
+              if brother_header and brother_header.next_sibling.name == "text"
+                brother_text = brother_header.next_sibling
+              end
+
+              containerify doc, [node, brother_header, brother_text].compact
+            end
+          end
+
+          # do it if the node's a <text>, and its immediate parent is a <section>
+          if (node.name == "text") and (node.parent.name == "section")
+            containerify doc, node
+          end
+
+          # do the same for some other nodes
+          if DISPLAY_BLOCKS.include?(node.name)
+            containerify doc, node
+          end
+        end
       end
 
     end
